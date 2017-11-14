1e15ba4092d21795c9f31c52a3ea91ff07a7f676
student <kajnak_robert@yahoo.com>
2017-11-14 13:54:28 +0100

    basic driver implemented

--- my_driver.py
+++ my_driver.py
@@ -1,11 +1,25 @@
 from pytocl.driver import Driver
-from pytocl.car import State, Command
+from pytocl.car import State, Command, MPS_PER_KMH
+import logging
 
+import math
 
 class MyDriver(Driver):
     # Override the `drive` method to create your own driver
-    ...
-    # def drive(self, carstate: State) -> Command:
+
+    def drive(self, carstate: State) -> Command:
     #     # Interesting stuff
-    #     command = Command(...)
-    #     return command
+         command = Command()
+         command.accelerator = 1
+         if carstate.rpm > 8000:
+            command.gear = carstate.gear + 1
+
+         if carstate.rpm < 2500:
+            command.gear = carstate.gear - 1
+
+         if not command.gear:
+            command.gear = carstate.gear or 1
+            
+            
+         return command
+ 

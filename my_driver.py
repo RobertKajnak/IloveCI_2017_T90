d00bb88bb9d299eb94e82f591a07933a729f5148
student <mirjamquaak@hotmail.com>
2017-11-24 09:52:05 +0100

    driver for MLP

--- my_driver.py
+++ my_driver.py
@@ -23,12 +23,15 @@ class Timer:
         print('%s took %f ms' %(self.name,(newtime-self.ctime)*1000))
         # self.ctime = newtime
 
-with open('ESN0.pkl', 'rb') as ESN_pickle:
-    my_esn = joblib.load(ESN_pickle)
+#with open('ESN0.pkl', 'rb') as ESN_pickle:
+    #my_esn = joblib.load(ESN_pickle)
 
-with open('regr.pkl', 'rb') as regr_pickle:
-    regr = joblib.load(regr_pickle)
+#with open('regr.pkl', 'rb') as regr_pickle:
+   # regr = joblib.load(regr_pickle)
    
+with open('mlp.pkl', 'rb') as mlp_pickle:
+    mlp = joblib.load(mlp_pickle)
+  
 class MyDriver(Driver):
     # Override the `drive` method to create your own driver
     my_esn = None;
@@ -38,49 +41,37 @@ class MyDriver(Driver):
     
     def __intit__(self):
         t = Timer();
-        with open('ESN0.pkl', 'rb') as ESN_pickle:
+        with open('mlp.pkl', 'rb') as ESN_pickle:
             self.my_esn = joblib.load(ESN_pickle)
 
         with open('regr.pkl', 'rb') as regr_pickle:
             self.regr = joblib.load(regr_pickle)
         t.clock()
         
-        
-    def drive(self, carstate: State) -> Command:
-        t = Timer('Input');
-        print('command recieved')
-    #     # Interesting stuff
+    def drive(self,carstate: State) -> Command:
         command = Command()
-        t.clock()
+        
         input = [carstate.speed_x, carstate.distance_from_center, carstate.angle] \
         + list(carstate.distances_from_edge)
-        echo_input = np.atleast_2d(input)
         
-        self.transTimer.reset()
-        echo_input = my_esn.transform(echo_input)
-        self.transTimer.clock()
-
-        self.regrTimer.reset()
-        echo_output = regr.predict(echo_input)
-        self.regrTimer.clock()
-        
-        if echo_output[0,0] > 1:
+        d_input = np.atleast_2d(input)
+        output = mlp.predict(d_input)
+        if output[0,0] > 1:
             accelerate = 1
-        elif echo_output[0,0] < 1:
+        elif output[0,0] < 1:
             accelerate = 0
         else:
-            accelerate = echo_output[0,0]
-        if echo_output[0,1] > 1:
+            accelerate = output[0]
+        if output[0,1] > 1:
             brake = 1
-        elif echo_output[0,1] < 1:
+        elif output[0,1] < 1:
             brake = 0
         else:
-            brake = echo_output[0,1]
-        print(accelerate, brake, echo_output[0,2])
+            brake = output[0,1]
+        print(accelerate, brake, output[0,2])
         command.accelerator = accelerate
         command.brake = brake
-        command.steering = echo_output[0, 2]
-                  
+        command.steering = output[0,2]
         #command.accelerator or acceleration?
                  
         #command.accelerator = 1
@@ -93,7 +84,57 @@ class MyDriver(Driver):
         if not command.gear:
             command.gear = carstate.gear or 1
         
-        print(command.gear)    
-            
+        print(command.gear)   
         return command
- 
+        
+        
+#    def drive(self, carstate: State) -> Command:
+#        t = Timer('Input');
+#        print('command recieved')
+#    #     # Interesting stuff
+#        command = Command()
+#        t.clock()
+#        input = [carstate.speed_x, carstate.distance_from_center, carstate.angle] \
+#        + list(carstate.distances_from_edge)
+#        echo_input = np.atleast_2d(input)
+#        
+#        self.transTimer.reset()
+#        echo_input = my_esn.transform(echo_input)
+#        self.transTimer.clock()
+#
+#        self.regrTimer.reset()
+#        echo_output = regr.predict(echo_input)
+#        self.regrTimer.clock()
+#        
+#        if echo_output[0,0] > 1:
+#            accelerate = 1
+#        elif echo_output[0,0] < 1:
+#            accelerate = 0
+#        else:
+#            accelerate = echo_output[0,0]
+#        if echo_output[0,1] > 1:
+#            brake = 1
+#        elif echo_output[0,1] < 1:
+#            brake = 0
+#        else:
+#            brake = echo_output[0,1]
+#        print(accelerate, brake, echo_output[0,2])
+#        command.accelerator = accelerate
+#        command.brake = brake
+#        command.steering = echo_output[0, 2]
+#                  
+#        #command.accelerator or acceleration?
+#                 
+#        #command.accelerator = 1
+#        if carstate.rpm > 8000:
+#            command.gear = carstate.gear + 1
+#
+#        if carstate.rpm < 2500 and carstate.gear > 0:
+#            command.gear = carstate.gear - 1
+#
+#        if not command.gear:
+#            command.gear = carstate.gear or 1
+#        
+#        print(command.gear)   
+#        return command         
+## 
